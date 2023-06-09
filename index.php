ebc3ff8cfee5746ce1de40231d760af759fd9b20
cenp86 <cenp86@gmail.com>
2023-06-09 10:34:35 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Simulador de préstamo</title>
+  <link rel="stylesheet" href="css/style.css">
+  <script type="text/javascript" src="../js/functions.js"></script>
+</head>
+<body>
+  <?php
+    $host = getenv("HOST");
+    echo $host.'<br>';   
+    echo 'hello world!!!';
+  ?>
+</body>
+</html>
